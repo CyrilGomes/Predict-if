0bddd7c9c8de85ea88aa73d6528cffbe1a839b01
MartinThibaud <martin.thibaud@dbmail.com>
2021-05-04 15:51:31 +0200

    Add draft of the frontend part

---
+++ Projet DASI - Frontend/src/main/java/com/thibaud/td2/servlet/ActionServlet.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thibaud.td2.servlet;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.projet.dasi.dao.JpaUtil;
+import com.projet.dasi.model.Utilisateur;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.projet.dasi.service.ServicesApplication;
+
+/**
+ *
+ * @author creep
+ */
+@WebServlet(urlPatterns = {"/ActionServlet"})
+public class ActionServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("json");
+        try (PrintWriter out = response.getWriter()) {
+            String typeRequete = (String) request.getParameter("todo");
+            if (typeRequete.equals("connecter")) {
+                ServicesApplication serviceApplication = new ServicesApplication();
+                Utilisateur u = serviceApplication.authentifier((String) request.getParameter("login"), (String) request.getParameter("password"));
+                
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                JsonObject result = new JsonObject();
+                result.addProperty("connexion", u != null); 
+                result.add("client", gson.toJsonTree(u));
+                out.println(gson.toJson(result));
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        JpaUtil.init();
+    }
+
+    @Override
+    public void destroy() {
+        JpaUtil.destroy();
+        super.destroy();
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
