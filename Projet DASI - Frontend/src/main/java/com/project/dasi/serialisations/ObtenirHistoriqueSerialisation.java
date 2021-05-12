c5c3c43b210ce0cdc4479b806dc8e0f85d3929e0
yaitichou <62092385+yaitichou@users.noreply.github.com>
2021-05-12 12:51:01 +0200

    Implémentation historique

--- Projet DASI - Frontend/src/main/java/com/project/dasi/serialisations/ObtenirHistoriqueSerialisation.java
+++ Projet DASI - Frontend/src/main/java/com/project/dasi/serialisations/ObtenirHistoriqueSerialisation.java
@@ -2,12 +2,19 @@ package com.project.dasi.serialisations;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.projet.dasi.model.Astrologue;
+import com.projet.dasi.model.Cartomancien;
 import com.projet.dasi.model.Client;
+import com.projet.dasi.model.Consultation;
 import com.projet.dasi.model.Employe;
+import com.projet.dasi.model.Medium;
+import com.projet.dasi.model.Spirite;
 import com.projet.dasi.model.Utilisateur;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,12 +34,44 @@ public class ObtenirHistoriqueSerialisation extends Serialisation {
         if (utilisateur instanceof Client) { container.addProperty("type", "client"); }
         if (utilisateur instanceof Employe) { container.addProperty("type", "employe"); }
         
+        
+        List<Consultation> historique = (List<Consultation>)request.getAttribute("historique");
+       
+        JsonArray historiqueJson = new JsonArray();
+        for(Consultation consultation : historique){
+            
+            Medium medium = consultation.getMedium();
+            JsonObject Consultation = new JsonObject();
+            
+            Consultation.addProperty("date", consultation.getDateDebut().toString());
+            Consultation.addProperty("genre", medium.getGenre().toString());
+            Consultation.addProperty("id", medium.getId());
+            Consultation.addProperty("presentation", medium.getPresentation());
+            
+            if(medium instanceof Astrologue){
+                Consultation.addProperty("formation", ((Astrologue)medium).getFormation());
+                Consultation.addProperty("promotion", ((Astrologue)medium).getPromotion());
+                Consultation.addProperty("typeMedium", "Astrologue");
+            }
+            else if(medium instanceof Spirite){
+                Consultation.addProperty("support", ((Spirite)medium).getSupport());
+                Consultation.addProperty("typeMedium", "Spirite");
+            }
+            else if(medium instanceof Cartomancien){
+                Consultation.addProperty("typeMedium", "Cartomancien");
+            }
+            historiqueJson.add(Consultation);
+        }
+        
+        container.add("historique", historiqueJson);
+        
+        
         // L'écrire sur le flux de sortie
         PrintWriter out = this.getWriter(response);
         Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
         gson.toJson(container, out);
-        out.close();        
-        
+        out.close();
+ 
     }
     
 }
