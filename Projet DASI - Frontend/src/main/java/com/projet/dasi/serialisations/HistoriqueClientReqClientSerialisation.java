6dd0eafd0c04ca4d6a2b77e4d27bf8ed1b4889ce
dt-mark <david.toma@insa-lyon.fr>
2021-05-13 15:39:32 +0200

    css historique vue client

--- Projet DASI - Frontend/src/main/java/com/projet/dasi/serialisations/HistoriqueClientReqClientSerialisation.java
+++ Projet DASI - Frontend/src/main/java/com/projet/dasi/serialisations/HistoriqueClientReqClientSerialisation.java
@@ -20,134 +20,134 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class HistoriqueClientReqClientSerialisation extends Serialisation {
-    
+
     @Override
     public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
         HttpSession session = request.getSession();
-        // Initialiser le container
         JsonObject container = new JsonObject();
-        
-        // Récupérer les attributs de la session
-        Utilisateur utilisateur = (Utilisateur)session.getAttribute("utilisateur");
-        
-        // Populer le container
+
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+
         container.addProperty("utilisateur", utilisateur != null);
-        
-        
-        List<Consultation> historique = (List<Consultation>)request.getAttribute("historique");
-       
+
+        List<Consultation> historique = (List<Consultation>) request.getAttribute("historique");
         JsonArray historiqueJson = new JsonArray();
-        for(Consultation consultation : historique){
-            
+        
+        for (Consultation consultation : historique) {
+
             Medium medium = consultation.getMedium();
-            JsonObject Consultation = new JsonObject();
-            
+            JsonObject consultationData = new JsonObject();
+
             Date date = consultation.getDateDebut();
             Calendar calendar = Calendar.getInstance();
             calendar.setTime(date);
+            
             // Récupération du jour du mois
             int jourDuMois = calendar.get(Calendar.DAY_OF_MONTH);
-            
+
             // Récupération du jour de la semaine
             int jourDeLaSemaine = calendar.get(Calendar.DAY_OF_WEEK);
-            
             String nomJour = null;
-            switch(jourDeLaSemaine){
+            switch (jourDeLaSemaine) {
                 case 1:
-			nomJour = "Dimanche";
-			break;
-		case 2:
-			nomJour = "Lundi";
-			break;
-		case 3:
-			nomJour = "Mardi";
-			break;
-		case 4:
-			nomJour = "Mercredi";
-			break;
-		case 5:
-			nomJour = "Jeudi";
-			break;
-		case 6:
-			nomJour = "Vendredi";
-			break;
-		case 7:
-			nomJour = "Samedi";
-			
+                    nomJour = "Dimanche";
+                    break;
+                case 2:
+                    nomJour = "Lundi";
+                    break;
+                case 3:
+                    nomJour = "Mardi";
+                    break;
+                case 4:
+                    nomJour = "Mercredi";
+                    break;
+                case 5:
+                    nomJour = "Jeudi";
+                    break;
+                case 6:
+                    nomJour = "Vendredi";
+                    break;
+                case 7:
+                    nomJour = "Samedi";
+
             }
             // Récupération du mois
-
             int mois = calendar.get(Calendar.MONTH);
-            
             String nomMois = null;
             switch (mois) {
-		case 0:
-			nomMois = "Janvier";
-			break;
-		case 1:
-			nomMois = "Février";
-			break;
-		case 2:
-			nomMois = "Mars";
-			break;
-		case 3:
-			nomMois = "Avril";
-			break;
-		case 4:
-			nomMois = "Mai";
-			break;
-		case 5:
-			nomMois = "Juin";
-			break;
-		case 6:
-			nomMois = "Juillet";
-			break;
-		case 7:
-			nomMois = "Août";
-			break;
-		case 8:
-			nomMois = "Septembre";
-			break;
-		case 9:
-			nomMois = "Octobre";
-			break;
-		case 10:
-			nomMois = "Novembre";
-			break;
-		case 11:
-			nomMois = "Decembre";
-			break;
+                case 0:
+                    nomMois = "Janvier";
+                    break;
+                case 1:
+                    nomMois = "Février";
+                    break;
+                case 2:
+                    nomMois = "Mars";
+                    break;
+                case 3:
+                    nomMois = "Avril";
+                    break;
+                case 4:
+                    nomMois = "Mai";
+                    break;
+                case 5:
+                    nomMois = "Juin";
+                    break;
+                case 6:
+                    nomMois = "Juillet";
+                    break;
+                case 7:
+                    nomMois = "Août";
+                    break;
+                case 8:
+                    nomMois = "Septembre";
+                    break;
+                case 9:
+                    nomMois = "Octobre";
+                    break;
+                case 10:
+                    nomMois = "Novembre";
+                    break;
+                case 11:
+                    nomMois = "Decembre";
+                    break;
             }
             
-            Consultation.addProperty("denomination", medium.getDenomination());
-            Consultation.addProperty("date",nomJour+" "+jourDuMois+" "+ nomMois);
-            Consultation.addProperty("genre", medium.getGenre().toString());
-            Consultation.addProperty("id", medium.getId());
-            Consultation.addProperty("presentation", medium.getPresentation());
+            // Récupération de l'année 
+            int annee = calendar.get(Calendar.YEAR);
             
-            if(medium instanceof Astrologue){
-                Consultation.addProperty("formation", ((Astrologue)medium).getFormation());
-                Consultation.addProperty("promotion", ((Astrologue)medium).getPromotion());
-                Consultation.addProperty("typeMedium", "Astrologue");
+            consultationData.addProperty("date", nomJour + " " + jourDuMois + " " + nomMois + " " + annee);
+            
+            JsonObject mediumData = new JsonObject();
+            mediumData.addProperty("denomination", medium.getDenomination());
+            mediumData.addProperty("genre", medium.getGenre().toString());
+            mediumData.addProperty("id", medium.getId());
+            mediumData.addProperty("presentation", medium.getPresentation());
+            if (medium instanceof Astrologue) {
+                mediumData.addProperty("formation", ((Astrologue)medium).getFormation());
+                mediumData.addProperty("promotion", ((Astrologue)medium).getPromotion());
+                mediumData.addProperty("type", "Astrologue");
+            } else if (medium instanceof Spirite) {
+                mediumData.addProperty("support", ((Spirite)medium).getSupport());
+                mediumData.addProperty("type", "Spirite");
+            } else if (medium instanceof Cartomancien) {
+                mediumData.addProperty("type", "Cartomancien");
             }
-            else if(medium instanceof Spirite){
-                Consultation.addProperty("support", ((Spirite)medium).getSupport());
-                Consultation.addProperty("typeMedium", "Spirite");
-            }
-            else if(medium instanceof Cartomancien){
-                Consultation.addProperty("typeMedium", "Cartomancien");
-            }
-            historiqueJson.add(Consultation);
+            consultationData.add("medium", mediumData);
+            
+            historiqueJson.add(consultationData);
+            
         }
-        
+
         container.add("historique", historiqueJson);
-        
+
         // L'écrire sur le flux de sortie
         PrintWriter out = this.getWriter(response);
         Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
         gson.toJson(container, out);
         out.close();
- 
+
     }
-    
+
 }
